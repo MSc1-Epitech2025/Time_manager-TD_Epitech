f857275ce364160e6310d6d042463b0509cf214e
AlexFraioli <alex.fraioli.af@gmail.com>
2025-10-10 14:42:51 +0200

    folder moved to /page + dashboard page in routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,13 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./pages/login/login').then(m => m.LoginComponent),
     },
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./pages/employee-dashboard/employee-dashboard').then(m => m.EmployeeDashboard),
+    },
     { path: '', pathMatch: 'full', redirectTo: 'login' },
-    { path: '**', redirectTo: 'login' }
+    { path: 'dashboard', pathMatch: 'full', redirectTo: 'dashboard' },
+    { path: '**', redirectTo: 'login' },
+
 ];
