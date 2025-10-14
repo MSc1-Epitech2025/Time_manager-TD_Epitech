9dec67f449d0ca7c0c8ed0eaa6f68741e6a094ff
Gaspard Malmon <malmon.gaspard@gmail.com>
2025-10-14 11:58:22 +0200

    first config for login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { TokenInterceptor } from './app/core/services/token-interceptor';
 
 const prefersDark = window.matchMedia?.('(prefers-color-scheme: dark)').matches;
 document.documentElement.classList.add(prefersDark ? 'theme-dark' : 'theme-light');
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideHttpClient(withInterceptors([TokenInterceptor]))
+  ]
+}).catch((err) => console.error(err));
