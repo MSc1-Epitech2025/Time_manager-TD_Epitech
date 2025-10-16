969e84214b14bfbdf6b1b53aadb39737a79ac482
AlexFraioli <alex.fraioli.af@gmail.com>
2025-10-16 15:41:30 +0200

    routes added without auth, dashboard enterprise + teams manager

---
+++ frontend/src/app/pages/log-history/log-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-history',
+  imports: [],
+  templateUrl: './log-history.html',
+  styleUrl: './log-history.scss'
+})
+export class LogHistory {
+
+}
