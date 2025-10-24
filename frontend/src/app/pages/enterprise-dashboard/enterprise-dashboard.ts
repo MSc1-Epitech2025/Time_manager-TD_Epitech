4c5baebdf30db08c1449b43fb3c389b727bda9fd
AlexFraioli <alex.fraioli.af@gmail.com>
2025-10-24 14:21:11 +0200

    minor changes

--- frontend/src/app/pages/enterprise-dashboard/enterprise-dashboard.ts
+++ frontend/src/app/pages/enterprise-dashboard/enterprise-dashboard.ts
@@ -184,8 +184,8 @@ export class EnterpriseDashboard implements OnDestroy {
     this.sessionStartTimestamp = undefined;
   }
   exportData(){
-    console.log('exportData button pressed')
-    this.reportService.exportEmployeeReport("op")
+    console.log('exportData button pressed') 
+    //this.reportService.exportEmployeeReport()
   }
 
   // utility to format timer as HH:MM:SS for template
