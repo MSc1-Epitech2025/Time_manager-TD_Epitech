626bb9a9c4dd75bd414b938405cd815311c318da
NH Bakli <86504182+NHBakli@users.noreply.github.com>
2025-12-01 17:12:47 +0100

    update environnement system

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  GRAPHQL_ENDPOINT: 'http://localhost:8030/graphql'
+  GRAPHQL_ENDPOINT: (window as any).env?.GRAPHQL_ENDPOINT,
+  AZURE_URL: (window as any).env?.AZURE_URL
 };
