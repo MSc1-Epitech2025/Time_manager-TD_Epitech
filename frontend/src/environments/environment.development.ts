f7c792c03475480cfa2bbfb026374656990d7190
Gaspard Malmon <malmon.gaspard@gmail.com>
2025-12-09 12:35:04 +0100

    Delete frontend/src/environments directory

--- frontend/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  GRAPHQL_ENDPOINT: 'http://localhost:8030/graphql'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  GRAPHQL_ENDPOINT: 'http://localhost:8030/graphql'
-};
