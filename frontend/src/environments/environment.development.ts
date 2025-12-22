716f1fe206c0cced926a69e613578afcf70299fe
NH Bakli <bakli25@protonmail.com>
2025-12-22 01:24:08 +0100

    update

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   GRAPHQL_ENDPOINT: 'http://localhost:8030/graphql',
-  AZURE_URL: 'http://localhost:8085'
+  JWT_EXP_MINUTES: 15,
+  JWT_REFRESH_DAYS: 45,
+  MAX_REFRESH_COUNT: 3,
+  AZURE_URL: 'http://localhost:8080'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
   GRAPHQL_ENDPOINT: 'http://localhost:8030/graphql',
-  AZURE_URL: 'http://localhost:8085'
+  JWT_EXP_MINUTES: 15,
+  JWT_REFRESH_DAYS: 45,
+  MAX_REFRESH_COUNT: 3,
+  AZURE_URL: 'http://localhost:8080'
 };
